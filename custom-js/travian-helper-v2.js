f8c744e7beae39f7793689a001ee964e03b3d47a
Desmond <desmond971220@gmail.com>
2022-12-23 02:11:47 +0800

    test

--- custom-js/travian-helper-v2.js
+++ custom-js/travian-helper-v2.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var _a, _b;
-const BUILD_TIME = "2022/12/23 02:04:25";
+const BUILD_TIME = "2022/12/23 02:10:44";
 const RUN_INTERVAL = 10000;
 const GID_NAME_MAP = {
     "-1": "Unknown",
@@ -754,6 +754,7 @@ const farm = (state, targetPrefix) => __awaiter(void 0, void 0, void 0, function
                 const href = $(ele).find('a').attr('href');
                 if (name === "Farm List") {
                     $(`a[href='${href}']`)[0].click();
+                    return;
                 }
             });
         }
